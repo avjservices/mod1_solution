6cb25dd2d276e8cf385536f56b616a1b63a95484
jyekmzone <jyekmzone@gmail.com>
2017-12-29 16:13:46 +0530

    module_1 assignment first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+    angular.module('LunchCheckApp', [])
+        .controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.checkMenu = function () {
+            if ($scope.order === "") {
+                $scope.responseMessage = "Please enter data first";
+            }
+            else {
+                var receivedOrder = $scope.order;
+                $scope.responseMessage = receivedOrder;
+                var menuItems = receivedOrder.split(',');
+                if (menuItems.length <= 3) {
+                    $scope.responseMessage = "Enjoy!"
+                }
+                else {
+                    $scope.responseMessage = "Too much!!"
+                }
+            }
+
+        };
+    }
+})();
